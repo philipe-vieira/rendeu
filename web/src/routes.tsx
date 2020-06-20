9c970badec6836f0991f89c20c8f089f97c96747
Philipe Vieira <philipefilho@hotmail.com>
2020-06-19 23:20:54 -0300

    creating the home page outline

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Route component={Home} path="/home" exact />
       <Route component={Login} path="/login" exact />
     </BrowserRouter>
   )
