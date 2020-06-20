8a286160fd19b4c4665a4dfd66aff6d2e35803e5
Philipe Vieira <philipefilho@hotmail.com>
2020-06-20 09:52:25 -0300

    Add icons and Link tag

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,44 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {  BsEnvelope, BsLock } from "react-icons/bs"
 
-import './styles.css';
+import './styles.css'
 import logoMarca from '../../assets/logo.png'
 
 const Login = () => {
   return (
     <div className="container">
       <img src={logoMarca} alt="logo"/>
+      
       <div className="login-container">
         <h3>Login</h3>
 
         <form>
           <div className="form-item">
-            <input type="email" name="email" id="" required/>
-            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" required />
+            <label htmlFor="email">
+              <BsEnvelope size="14" /> Email
+            </label>
           </div>
 
           <div className="form-item">
-            <input type="password" name="password" id="" required/>
-            <label htmlFor="senha">Senha</label>
+            <input type="password" name="password" id="" required />
+            <label htmlFor="senha">
+              <BsLock size="14" /> Senha
+            </label>
           </div>
 
-          <button type="submit">Enviar</button>
+          <button type="submit">Login</button>
 
         </form>
 
         <p>
-          <a 
-            href="http://" 
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            Esqueceu a senha? 
-          </a> Ou <a 
-            href="http://" 
-            target="_blank" 
-            rel="noopener noreferrer"
-          >
-            Cadastre-se
-          </a>
+          <Link to="/">Esqueceu a senha?</Link> Ou <Link to="/register">Cadastre-se</Link>
         </p>
       </div>
     </div>
